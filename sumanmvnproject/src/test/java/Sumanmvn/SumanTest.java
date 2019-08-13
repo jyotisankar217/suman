eb2beb6c9b8a4ee1a3a73e631a459e1eaa07b50e
jyotisankar217 <jyotis.barik@gmail.com>
2019-08-14 00:05:07 +0530

    first commit

---
+++ sumanmvnproject/src/test/java/Sumanmvn/SumanTest.java
@@ -0,0 +1,18 @@
+package Sumanmvn;
+
+import org.testng.annotations.Test;
+
+public class SumanTest {
+@Test
+public void suman1 ()
+      {
+	System.out.println("Test successfully");
+	System.out.println("Welcome to TestNG");
+	  System.out.println("Welcome to manual testing"); 
+	  System.out.println("Welcome to selenium");
+     }
+		  
+
+	    	 
+	    	  }
+
