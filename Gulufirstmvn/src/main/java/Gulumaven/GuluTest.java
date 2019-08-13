615e46d651600126a18db2c3755fd3a6f704374b
jyotisankar217 <jyotis.barik@gmail.com>
2019-08-14 00:46:56 +0530

    first commit

---
+++ Gulufirstmvn/src/main/java/Gulumaven/GuluTest.java
@@ -0,0 +1,12 @@
+package Gulumaven;
+
+
+
+public class GuluTest {
+	 public void testng3(){
+		  System.out.println("Welcome to TestNG2");
+		  System.out.println("Welcome to manual testing2"); 
+		  System.out.println("Welcome to selenium2");
+
+ }
+}
